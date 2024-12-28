8914c5dbb02dfd6a196eb0e138ff40df87d6ca06
Anselbiju <anselbiju5@gmail.com>
2024-12-28 10:43:38 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,65 @@
+# Topic :List Exercise. 
+# Q1. Create a list of 5 random numbers and print the list. 
+# Q2. Insert 3 new values to the list and print the updated list. 
+# Q3. Try to use a for loop to print each element in the list. 
+
+# Topic: Dictionary Exercise. 
+# Q1. Create a dictionary with keys 'name', 'age', and 'address' and values 'John', 25, and 'New York' respectively.
+# Q2. Add a new key-value pair to the dictionary created in Q1 with key 'phone' and value '1234567890'. 
+
+# Topic: Set Exercise 
+# Q1. Create a set with values 1, 2, 3, 4, and 5. 
+# Q2. Add the value 6 to the set created in Q1. 
+# Q3. Remove the value 3 from the set created in Q1. 
+
+# Topic:Tuple Exercise . 
+# Q1. Create a tuple with values 1, 2, 3, and 4 
+# Q2. Print the length of the tuple created in Q1.
+ ########################################################################################
+
+# Topic :List Exercise.
+# Q1
+# import random
+# list1 = [random.randint(1, 10) for n in range(5)]
+# print(list1)
+
+# Q2
+# list1.append(10)
+# list1.append(20)
+# list1.append(30)
+# print(list1)
+
+# Q3
+# for i in list1:
+#     print(i)
+
+
+# topic : Dictionary Exercise.
+# Q1
+# dictnary1 ={'name': 'ansel','age':24,'address':'america'}
+# print(dictnary1)
+
+# Q2
+# dictnary1['phone'] = '1234567890'
+# print(dictnary1)
+
+# Topic : Set Exercise
+# Q1
+# set1 = {1,2,3,4,5}
+# print(set1)
+
+# Q2
+# set1.add(6)
+# print(set1)
+
+# Q3
+# set1.remove(3)
+# print(set1)
+
+# Topic : Tuple Exercise
+# Q1
+# s = (1,2,3,4)
+# print(s)
+
+# Q2
+# print(len(s))
